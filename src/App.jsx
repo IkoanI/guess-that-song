fcaeb51d321eaf7f18fcaddf609cea4ba23d573e
Koan Yeng <koan329@gmail.com>
2023-06-08 21:10:46 -0400

    fixed naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom"
-import Login from "./pages/login"
+import Login from "./pages/LoginPage"
 import Loading from "./pages/Loading"
 import Quiz from "./pages/Quiz";
 export const clientId = "ec1d519601d9404daf77057dec294452";
